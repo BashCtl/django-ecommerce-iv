f3a6d661ce265cd101c5b2569ee124c6dec7e676
darko <ki14.skakun@gmail.com>
2023-11-30 10:47:49 +0200

    product image field

--- store/models.py
+++ store/models.py
@@ -18,11 +18,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=True)
-    # image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='images')
 
     def __str__(self):
         return self.name
-
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(
